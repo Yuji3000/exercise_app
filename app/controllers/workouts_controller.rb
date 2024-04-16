ce3cfcfd2a56c6c50c32739e16fc49922e8820fc
Yuji Kosakowski <yuji.kosa@gmail.com>
2024-04-16 13:48:03 -0600

    Add links for programs and adding a workout entry, plus routes/views

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,5 @@
+class WorkoutsController < ApplicationController
+  def index
+    # @user = User.find_by_id(params[:user_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create(name: "first user", email: "first@user.com", height_inches: 51 , body_weight: 151)
+program1 = Program.create(name: "New Years Pump Up", description: "Doin cool stuff this year")
+user_program1 = UserProgram.create(user_id: user1.id, program_id: program1.id)
+workout1 = Workout.create(name: "Chest", day_of_week: "monday", program_id: user_program1)
+workout2 = Workout.create(name: "Back", day_of_week: "wednesday", program_id: user_program1)
+workout3 = Workout.create(name: "Legs", day_of_week: "friday", program_id: user_program1)
